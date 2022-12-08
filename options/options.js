57e4c83d40a6d1b05ffd61a32d925f4152c3357e
Crowdbotics <team@crowdbotics.com>
2022-12-08 15:13:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "cooperation_island_37829",
+  url: "https://cooperation_island_37829.botics.co",
+  api: "https://cooperation_island_37829.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
